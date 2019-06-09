38d1706858fc7c8e048b54868da0238f0b0bbfd9
Ilya13 <shibaev.ilya@4px.ru>
2019-06-09 23:03:43 +0300

    активные шаги

--- js/script.min.js
+++ js/script.min.js
@@ -1,6 +1,12 @@
 var buy = document.querySelector('.button');
 var modalWindow = document.querySelector('.modal');
 var closeModalWindow = document.querySelector('.close');
+var forward = document.querySelector('.forward');
+var back = document.querySelector('.back');
+var two = document.querySelector('.two');
+var one = document.querySelector('.one');
+var three = document.querySelector('.three');
+var four = document.querySelector('.four');
 
 buy.addEventListener('click', function(evt) {
     evt.preventDefault();
@@ -11,6 +17,29 @@ closeModalWindow.addEventListener('click', function() {
     modalWindow.classList.remove('visible');
 });
 
+var countStep = 0;
+
+forward.addEventListener('click', function() {
+    countStep +=1;
+    console.log(countStep);
+    if(countStep == 1 ) {
+        two.classList.add('active');
+        one.classList.remove('active');
+        one.classList.add('done'); 
+    }
+    if (countStep == 2 ) {
+        three.classList.add('active');
+        two.classList.remove('active');
+        two.classList.add('done');
+    }
+    if (countStep == 3 ) {
+        four.classList.add('active');
+        three.classList.remove('active');
+        three.classList.add('done');
+        forward.textContent = "Подписаться";
+    }
+    
+});
 
 
 
